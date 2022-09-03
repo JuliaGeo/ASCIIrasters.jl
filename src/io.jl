772c0c2323f7c4f77bf7887e05ef12b0cba6c44b
Josquin Guerber <95242763+jguerber@users.noreply.github.com>
2022-09-03 16:21:59 +0200

    Detect data type while reading and force conversions while writing (#4)
    * Type detection based on nodata type
    
    * kwarg for type detection when writing
    
    * Test type detection
    
    * Float64 falls back to Float32
    
    * Restrict kw argument types
    
    * Adapt docstring
    
    * explicit types in tests

--- src/io.jl
+++ src/io.jl
@@ -1,13 +1,13 @@
 """
     read_ascii(filename::AbstractString) => Union{Tuple{Array, NamedTuple}, NamedTuple}
 
-Reads an ASCII file. Parameters are parsed according to the [AAIGrid](https://gdal.org/drivers/raster/aaigrid.html) format.
+Reads an ASCII file. Parameters are parsed according to the [AAIGrid](https://gdal.org/drivers/raster/aaigrid.html) format. Data elements are assumed to be of the same type as the nodatavalue header parameter.
 
 # Keywords
 
  - `lazy`: when set to `true`, only the header of `filename` will be read, and only the `NamedTuple` of parameters be returned.
 
-If not `lazy`, Returns a `Tuple` with: an `Array` of the data and a `NamedTuple` of the header information.
+If not `lazy`, returns a `Tuple` with: an `Array` of the data and a `NamedTuple` of the header information.
 """
 function read_ascii(filename::AbstractString; lazy = false)
     isfile(filename) || throw(ArgumentError("File $filename does not exist"))
@@ -18,15 +18,19 @@ function read_ascii(filename::AbstractString; lazy = false)
         yll = parse(Float64, match(r"yllcorner (.+)", readline(file)).captures[1])
         dx = parse(Float64, match(r"dx (.+)", readline(file)).captures[1])
         dy = parse(Float64, match(r"dy (.+)", readline(file)).captures[1])
-        NA = parse(Float64, match(r"NODATA_value (.+)", readline(file)).captures[1])
+        na_str = match(r"NODATA_value (.+)", readline(file)).captures[1]
+
+        # no floating point in nodata ? datatype is considered int
+        datatype = isnothing(match(r"[.]", na_str)) ? Int32 : Float32
+        NA = parse(datatype, na_str)
 
         params = (nrows = nr, ncols = nc, xll = xll, yll = yll, dx = dx, dy = dy, nodatavalue = NA)
 
         if !lazy
-            out = Array{Float64}(undef, nr, nc)
+            out = Array{datatype}(undef, nr, nc)
 
             for row in 1:nr
-                out[row, :] = parse.(Float64, split(readline(file), " ")[2:end]) # data lines start with a space
+                out[row, :] = parse.(datatype, split(readline(file), " ")[2:end]) # data lines start with a space
             end
             output = (out, params)
         else
@@ -37,8 +41,8 @@ function read_ascii(filename::AbstractString; lazy = false)
     return output
 end
 
-function write_ascii(filename::AbstractString, dat::AbstractArray, args::NamedTuple)
-    write_ascii(filename, dat; args...)
+function write_ascii(filename::AbstractString, dat::AbstractArray, args::NamedTuple; kwargs...)
+    write_ascii(filename, dat; args..., kwargs...)
 end
 
 """
@@ -57,10 +61,23 @@ Writes data and header in an [AAIGrid](https://gdal.org/drivers/raster/aaigrid.h
  - `dx` and `dy`: dx and dy cell sizes in coordinate units per pixel
  - `nodatavalue`: a value that should be considered as holding no data
 
+ - `detecttype`: when set to `true`, elements of `dat` are assumed to be of the same type as `nodatavalue`. Leave `false` to coerce everything (both `dat` and `nodatavalue` to `Float32`).
+
 Returns the written file name.
 """
-function write_ascii(filename::AbstractString, dat::AbstractArray{T, 2}; ncols, nrows, xll, yll, dx, dy, nodatavalue) where T
+function write_ascii(filename::AbstractString, dat::AbstractArray{T, 2}; ncols::Int, nrows::Int, xll::Real, yll::Real, dx::Real, dy::Real, nodatavalue::Union{AbstractFloat, Int32, Int64}, detecttype = false) where T
     size(dat) == (nrows, ncols) || throw(ArgumentError("$nrows rows and $ncols cols incompatible with array of size $(size(dat))"))
+
+    datatype = if detecttype
+        typeof(nodatavalue) <: AbstractFloat ? Float32 : Int32 
+    else
+        Float32
+    end
+    
+    # ensure right type for dat and nodatavalue
+    dat = datatype.(dat)
+    nodatavalue = datatype(nodatavalue)
+
     # Write
     open(filename, "w") do f
         write(f,
